3d2b0fa67bed4fade24b69411fc76c918b2f8395
Joel <joeleoj2006@gmail.com>
2026-02-17 16:13:04 -0600

    eliminado imagenes del repo, agregado link de imagenes en linea

--- Lab 6 programacion orientada a objetos/js/dom.js
+++ Lab 6 programacion orientada a objetos/js/dom.js
@@ -4,7 +4,8 @@ const mclaren = {
     color: "#F47600",
     piloto1: "Lando Norris",
     piloto2: "Oscar Piastri",
-    img: "imagenes/McLaren.avif",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/McLaren/MCL40_C_OP_Social_1920x1080.webp",
+    link: "https://www.mclaren.com/racing/formula-1/team/",
 }
 
 const ferrari = {
@@ -13,7 +14,8 @@ const ferrari = {
     color: "#ED1131",
     piloto1: "Charles Leclerc",
     piloto2 : "Lewis Hamilton",
-    img: "imagenes/ferrari.avif",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Ferrari/G_V027BWQAASNgK.webp",
+    link: "https://www.ferrari.com/en-EN/formula1",
 }
 
 const mercedes = {
@@ -22,7 +24,8 @@ const mercedes = {
     color: "#00D7B6",
     piloto1: "George Russel",
     piloto2 : "Kimi Antonelli",
-    img: "imagenes/mercedes.avif",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Mercedes/Mercedes-AMG%20F1%20W17%20E%20PERFORMANCE%20-%20GR%206.webp",
+    link: "https://www.mercedesamgf1.com",
 }
 
 const williams = {
@@ -31,7 +34,8 @@ const williams = {
     color: "#1868DB",
     piloto1: "Alex Albon",
     piloto2 : "Carlos Sainz",
-    img: "imagenes/williams.avif",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Williams/f34h_v4-Sainz_169.webp",
+    link: "https://www.williamsf1.com",
 }
 
 const haas = {
@@ -40,7 +44,8 @@ const haas = {
     color: "#9C9FA2",
     piloto1: "Oliver Bearman",
     piloto2 : "Esteban Ocon",
-    img: "imagenes/haas.avif",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Haas/10-Photos-16x9-EO.0002.webp",
+    link: "https://www.haasf1team.com",
 }
 
 const redbull = {
@@ -48,8 +53,9 @@ const redbull = {
     longName : "Oracle Red Bull Racing",
     color: "#3671C6",
     piloto1: "Max Verstappen",
-    piloto2: "Liam Lawson",
-    img: "imagenes/red bull.avif",
+    piloto2: "Isaac Hadjar",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Red%20Bull/SI202601150723.webp",
+    link: "https://www.redbullracing.com/int-en"
 }
 
 const astonmartin = {
@@ -58,7 +64,8 @@ const astonmartin = {
     color: "#229971",
     piloto1: "Fernando Alonso",
     piloto2: "Lance Stroll",
-    img: "imagenes/aston martin.avif",
+    img: "https://media.formula1.com/image/upload/t_16by9Centre/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Aston%20Martin/GD2_2384%20copy.webp",
+    link: "https://www.astonmartinf1.com/en-GB",
 }
 
 const alpine = {
@@ -66,17 +73,19 @@ const alpine = {
     longName : "BWT Alpine F1 Team",
     color: "#0093CC",
     piloto1: "Pierre Gasly",
-    piloto2: "Jack Doohan",
-    img: "imagenes/alpine.avif",
+    piloto2: "Franco Colapinto",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Alpine/TWJB_BWT_ALPINE_FORMULA_ONE_TEAM-A526_HERO_SHOT_2.webp",
+    link: "https://www.alpinef1.com",
 }
 
 const rb = {
     shortName : "VCARB",
     longName : "Visa Cash App RB Formula One Team",
     color: "#6692FF",
-    piloto1: "Yuki Tsunoda",
-    piloto2: "Isack Hadjar",
-    img: "imagenes/racing bulls.avif",
+    piloto1: "Liam Lawson",
+    piloto2: "Arvid lindblad",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Racing%20Bulls%20(VCARB)/SI202601151081.webp",
+    link: "https://www.visacashapprb.com/int-en",
 }
 
 const audi = {
@@ -85,7 +94,8 @@ const audi = {
     color: "#F50537",
     piloto1: "Nico Hülkenberg",
     piloto2: "Gabriel Bortoleto",
-    img: "imagenes/audi.avif",
+    img: "https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Audi/16x9%20single%20image%20(39).webp",
+    link: "https://www.audif1.com/en",
 }
 
 const cadillac = {
@@ -94,7 +104,8 @@ const cadillac = {
     color: "#FFFFFF",
     piloto1: "Sergio Perez",
     piloto2: "Valteri Bottas",
-    img: "imagenes/cadillac.avif",
+    img: "https://media.formula1.com/image/upload/t_16by9Centre/c_lfill,w_3392/q_auto/v1740000000/fom-website/2026/Cadillac/CadillacF1Team_2203_HiRes.jpg.webp",
+    link: "https://www.cadillacf1team.com",
 }
 
 // Array de equipos con sus ID de div correspondientes
@@ -120,6 +131,7 @@ const on_hover = (team, divTeam) => {
         </figure>
     `;
 }
+
 const off_hover = (team, divTeam) => {
     divTeam.innerHTML = `
         <div style="color: ${team.color}">${team.shortName}</div>
