1b361fcd6eefbbf6c9d7023f544259d4afdc16fc
Joel <joeleoj2006@gmail.com>
2026-02-24 14:26:27 -0600

    Fix

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log("Hello world")
-console.log("Hello bro")
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const rutas_f1teams = require('./routes/f1Teams.routes');
+app.use('/teams', rutas_f1teams);
+
+app.use((request, response, next) => {
+    response.status(404).send("La ruta no existe");
+})
+
+app.listen(3000);
