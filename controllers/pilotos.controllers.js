83be99fe58cd23789b0662b55a71526f6bc0d191
Joel <joeleoj2006@gmail.com>
2026-03-02 20:02:36 -0600

    Agregar controladores a proyecto, fin de lab 13

--- controllers/pilotos.controllers.js
+++ controllers/pilotos.controllers.js
@@ -7,7 +7,3 @@ exports.get_verstappen = (request, response, next) => {
 exports.get_piastri = (request, response, next) => {
     response.render('../views/oscar'); //Manda la respuesta
 };
-
-//aqui agrego lo que pasa cuando entro a tal ruta /quiz
-
-//en el archivo teams.routes agrego el require de este archivo
--- routes/f1Drivers.routes.js
+++ routes/f1Drivers.routes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const teams = require('../models/equipos.model')
 
-const teamsController = require('../controllers/equipos.controllers');
+const pilotosController = require('../controllers/pilotos.controllers');
 //Necesito de agregar la variable que contenga el archivo el cual va a agregar una variable constante que agrege todo lo que exporte el archivo
 //tomo la funciones que requieren unos / necesarios y se la agrego a el archivo equipos.controlles
 //y necesito de agrega el archivo y le escribo la variable que contiene la funcion equipos.controllers.get_add (cualquier nombre que le quiera agregar)
 
-router.get('/mv1', teamsController.get_verstappen);
-router.get('/op81', teamsController.get_piastri);
+router.get('/mv1', pilotosController.get_verstappen);
+router.get('/op81', pilotosController.get_piastri);
 
 //esto es lo que le manda A a donde se necesite llamado como B
 //Osea que manda A (teams) a las partes que lo necesiten y lo manda como una
@@ -18,10 +18,6 @@ router.use((request, response, next) => {
     const listaEquipos = teams.fetchAll();
     response.render('drivers', {teams: listaEquipos}); 
     next();
-});
-
-router.use((request, response, next) => {
-    response.render('drivers.ejs');
 });
 
 module.exports = router;
